82338b0fef45c51b1ac23d2e88cc9b6a132b4580
Leigh966 <leigh966@hotmail.com>
2024-11-02 20:14:39 +0000

    added basic present dropping mechanic

--- Assets/SantaMovement.cs
+++ Assets/SantaMovement.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class SantaMovement : MonoBehaviour
 {
     public Transform rudolf;
+    public GameObject presentPrefab;
+    public Transform sleigh;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,5 +21,10 @@ public class SantaMovement : MonoBehaviour
         Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition, Camera.MonoOrStereoscopicEye.Mono);
         rudolf.position = new Vector3(rudolf.position.x, mouseWorldPos.y+Mathf.Sin(Time.realtimeSinceStartup*2)/2f, rudolf.position.z);
         transform.position = new Vector3(mouseWorldPos.x, transform.position.y, 0f);
+        if(Input.GetMouseButtonDown(0))
+        {
+            GameObject newPres = Instantiate(presentPrefab);
+            newPres.transform.position = sleigh.position;
+        }
     }
 }
