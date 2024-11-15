d4253fb6294e96a2b23e12a009eef1116b6bd77a
Leigh966 <leigh966@hotmail.com>
2024-11-15 23:23:06 +0000

    added speed changing zones to give the player some control over the scroll speed - this has messed with the physics of the presents so that will need fixing

---
+++ Assets/SpeedChangeAreaBehaviour.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody2D))]
+public class SpeedChangeAreaBehaviour : MonoBehaviour
+{
+    public float speedDelta;
+    public float maxSpeed, minSpeed;
+    public ScrollingBehaviour scroller;
+    public LayerMask layersToDetect;
+
+
+    private int colliding;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        colliding = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (colliding <= 0) return;
+        scroller.scrollSpeed = Mathf.Clamp(scroller.scrollSpeed + speedDelta * Time.deltaTime, minSpeed, maxSpeed);   
+    }
+
+    private bool CollidingWithTargets(int layer)
+    {
+        return (layersToDetect & (1 << layer)) != 0;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (CollidingWithTargets(collision.gameObject.layer))
+        {
+            Debug.Log(colliding);
+            colliding++;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (CollidingWithTargets(collision.gameObject.layer))
+        {
+            colliding--;
+        }
+    }
+}
