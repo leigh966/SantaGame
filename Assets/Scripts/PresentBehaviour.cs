a53d6dfe6e7ab498f7fbe87bb085121dbd2b8b8a
Leigh966 <leigh966@hotmail.com>
2024-11-28 14:51:59 +0000

    tweaked present physics and added animated sprites to reindeer

--- Assets/Scripts/PresentBehaviour.cs
+++ Assets/Scripts/PresentBehaviour.cs
@@ -8,28 +8,31 @@ public class PresentBehaviour : ScrollingBehaviour
 {
     public float leftwardForce;
     private Rigidbody2D rb;
-    private bool applyForce = true;
+    private bool applyForce;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        applyForce = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(!applyForce)
-        {
-            transform.Translate(Vector2.left*Time.deltaTime*scrollSpeed);
-        }
+        //if(!applyForce)
+        //{
+        Vector2 movement = Vector2.left * Time.deltaTime * scrollSpeed;
+        transform.Translate(movement);
+
+        //}
     }
 
     private void FixedUpdate()
     {
-        float forceMagnitude = leftwardForce * Convert.ToInt32(applyForce);
-        Debug.Log(forceMagnitude);
-        rb.AddForce(Vector2.left*forceMagnitude);
+        //float forceMagnitude = leftwardForce * Convert.ToInt32(applyForce);
+        //Debug.Log(forceMagnitude);
+        //rb.AddForce(Vector2.left*forceMagnitude);
     }
 
     private void CancelLeftDrift()
