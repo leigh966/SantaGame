5bd7a9e985f643e5ec221cde3580984763ee7e1d
Leigh966 <leigh966@hotmail.com>
2024-12-01 21:19:38 +0000

    updated present physics to have a more realistic fall

--- Assets/Scripts/PresentBehaviour.cs
+++ Assets/Scripts/PresentBehaviour.cs
@@ -7,41 +7,24 @@ using UnityEngine;
 public class PresentBehaviour : ScrollingBehaviour
 {
     public float leftwardForce;
-    private Rigidbody2D rb;
-    private bool applyForce;
 
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
-        applyForce = true;
+        GetComponent<Rigidbody2D>().velocity = Vector2.right*scrollSpeed;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        //if(!applyForce)
-        //{
-        Vector2 movement = Vector2.left * Time.deltaTime * scrollSpeed;
-        transform.Translate(movement);
 
-        //}
     }
 
     private void FixedUpdate()
     {
-        //float forceMagnitude = leftwardForce * Convert.ToInt32(applyForce);
-        //Debug.Log(forceMagnitude);
-        //rb.AddForce(Vector2.left*forceMagnitude);
+
     }
 
-    private void CancelLeftDrift()
-    {
-        applyForce = false;
-    }
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        CancelLeftDrift();
-    }
 }
