881d6fe3e6129c6fb4b193840c27fc462b6b2edd
leigh966 <leigh966@hotmail.com>
2024-12-15 16:05:51 +0000

    added present drop cooldown

--- Assets/Scripts/SantaMovement.cs
+++ Assets/Scripts/SantaMovement.cs
@@ -7,12 +7,15 @@ public class SantaMovement : MonoBehaviour
     public Transform rudolf;
     public List<GameObject> presentPrefabs;
     public Transform sleigh;
+    public float parcelCooldown;
+
+    private bool canDrop;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        canDrop = true;
     }
 
     private GameObject ChooseRandomPresent()
@@ -21,11 +24,17 @@ public class SantaMovement : MonoBehaviour
         return presentPrefabs[index];
     }
 
+    private void EnableDrop()
+    {
+        canDrop = true;
+    }
 
     void DropPresent()
     {
         GameObject newPres = Instantiate(ChooseRandomPresent());
         newPres.transform.position = sleigh.position;
+        canDrop = false;
+        Invoke("EnableDrop", parcelCooldown);
     }
 
     // Update is called once per frame
@@ -34,7 +43,7 @@ public class SantaMovement : MonoBehaviour
         Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition, Camera.MonoOrStereoscopicEye.Mono);
         rudolf.position = new Vector3(rudolf.position.x, mouseWorldPos.y+Mathf.Sin(Time.realtimeSinceStartup*2)/2f, rudolf.position.z);
         transform.position = new Vector3(mouseWorldPos.x, transform.position.y, 0f);
-        if(Input.GetMouseButtonDown(0))
+        if(canDrop && Input.GetMouseButtonDown(0))
         {
             DropPresent();
         }
