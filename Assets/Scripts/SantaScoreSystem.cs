472fbb7aa26e9955b44192958902a0901ff39e26
leigh966 <leigh966@hotmail.com>
2025-12-17 16:27:48 +0000

    added sound effects for points scored

--- Assets/Scripts/SantaScoreSystem.cs
+++ Assets/Scripts/SantaScoreSystem.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+[RequireComponent(typeof(AudioSource))]
 public class SantaScoreSystem : ScoreSystem
 {
     public Animator santa;
 
     public string PrefToSet;
+
+    public List<AudioClip> beepSounds;
 
     public int Score
     {
@@ -22,11 +24,19 @@ public class SantaScoreSystem : ScoreSystem
         PlayerPrefs.SetInt(PrefToSet, score);
     }
 
+
+    private AudioClip RandomBeep()
+    {
+        int index = Random.Range(0, beepSounds.Count);
+        return beepSounds[index];
+    }
+
     public override void IncrementScore()
     {
         base.IncrementScore();
 
         santa.SetTrigger("Goal");
+        GetComponent<AudioSource>().PlayOneShot(RandomBeep());
 
         PlayerPrefs.SetInt(PrefToSet, score);
 
